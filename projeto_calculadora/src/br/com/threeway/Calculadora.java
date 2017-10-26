66e29b95161f8bffedb06d88c46d330e0d5d2509
dyegohitalo <30508486+dyegohitalo@users.noreply.github.com>
2017-10-26 14:17:41 -0200

    Update Calculadora.java

--- projeto_calculadora/src/br/com/threeway/Calculadora.java
+++ projeto_calculadora/src/br/com/threeway/Calculadora.java
@@ -1,10 +1,81 @@
 package br.com.threeway;
-
+import java.util.Scanner;
 public class Calculadora {
-	private String nome = "Calc dos Alunos";
 	
-	public int somar (int numero1, int numero2) {
+	//Declarando os métodos
+	
+	public int somar(int num1, int num2) {
+        return num1 + num2;
+    }  
+    public int subtrair(int num1, int num2){
+        return num1 - num2;
+    }  
+    public int dividir(int num1,int num2){
+        return num1 / num2;
+    }  
+    public int multiplicar(int num1, int num2){
+        return num1 * num2;
+    }  
+    
+    public static void main (String args[]){ 
+        
+    	//Criando um objeto c a apartir do método calculadora  
+        
+    	Calculadora c = new Calculadora();  
+        
+        //Declarando as variáveis  
+    	
+        int opcao = 5;  
+        int num1;  
+        int num2;  
 		
-	}
-
+        @SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+        
+        System.out.println("-> Escolha uma operação matemática <-");  
+        System.out.println("1. Somar");    
+        System.out.println("2. Subtrair");    
+        System.out.println("3. Multiplicar");    
+        System.out.println("4. Dividir");    
+        System.out.println("0. Sair");    
+        System.out.println("Operação: ");    
+        
+        opcao = input.nextInt();  
+        
+        while (opcao != 0) {
+			@SuppressWarnings("resource")
+			Scanner input1 = new Scanner(System.in);    
+			System.out.println("Informe o primeiro Nº: ");  
+			num1 = input1.nextInt();  
+			System.out.println("Informe o Segundo Nº: ");  
+			num2 = input1.nextInt();
+			
+			if ( opcao == 1 ) {
+				int operacao = c.somar(num1, num2);
+                System.out.printf("\nO resultado da soma é: %d\n", operacao);  
+                break;  
+            } 
+			
+            else if (opcao == 2) { 
+				int operacao = c.subtrair(num1, num2);
+				System.out.printf("\nO resultado da subtração é: %d\n", operacao);  
+				break;  
+			}
+			
+			else if (opcao == 3) {
+				int operacao = c.multiplicar(num1, num2); 
+				System.out.printf("\nO resultado da multiplicação é: %d\n", operacao);    
+				break;  
+			}  
+			else if (opcao == 4) {  
+				int operacao = c.dividir(num1, num2);  
+				System.out.printf("\nO resultado da divisão é: %d\n", operacao);   
+				break;  
+			}  
+			else{  
+				System.out.println("ERROR: Operação escolhida é inválida!!!");  
+				break;  
+			}      
+        } // Fim do while - Usuário optou por sair
+    }  // Finalizando do método principal
 }
